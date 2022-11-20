e61d57022e9a23e8c32e4aa1ab25fde598fee123
23456-Tony-Larrea <avs.larrea@yavirac.edu.ec>
2022-11-20 10:41:07 -0500

    todo

---
+++ js/question.js
@@ -0,0 +1,37 @@
+
+//Array of objects(qustions)
+
+
+const quiz =[
+{
+	q:'Is HTML a programming language?',
+	options:['Yes','No'],
+	answer:1
+},
+{
+	q:'What is one of the reasons Java is different to Javascript?',
+	options:['Java is compiled before it runs and isn\'t limited to being on a web browser.','Java is just short for javascript','Javascript is barely used anymore since Java came out.'],
+	answer:0
+},
+{
+	q:'What is the newest version of HTML being developed?',
+	options:['HTML4','HTML6','HTML5','There is only one type of HTML'],
+	answer:2
+},
+{
+	q:'What does CSS stand for?',
+	options:['Complete Sound Sheet','Cascading Style Sheet','Cscading Style Syntax'],
+	answer:1
+},
+{
+	q:'Non animated gifs are usually lower in file size, why don\'t people use them for photos',
+	options:["Because it hasn't caught on.",'You need to pay for a lisencse to use them.','They are distracting and decrease Usability.','None of the above'],
+	answer:2
+},
+{
+	q:"What happens to a lot of images saved as jpgs when you put them online.",
+	options:['Stretching',"They'll turn blur",'They are too big in file size.','Lower quality and artifacts.'],
+	answer:3
+}
+]
+
